12a176ba5415250d6a1167b69ddad369ea4e0684
Amund Tenstad <github@amund.io>
2024-11-27 12:14:18 +0100

    fix: add omitempty to optional group fields (#41)

--- apis/user/v1alpha1/group_types.go
+++ apis/user/v1alpha1/group_types.go
@@ -30,7 +30,7 @@ type GroupParameters struct {
 	// Active determines whether the group is enabled ("true") or disabled ("false") in the system.
 	//+optional
 	//+kubebuilder:validation:Pattern=`^(true|false)$`
-	Active *string `json:"active"`
+	Active *string `json:"active,omitempty"`
 	// GroupID is the group ID (known as Name in the GUI).
 	//+kubebuilder:validation:MinLength=1
 	//+kubebuilder:validation:MaxLength=64
@@ -39,32 +39,32 @@ type GroupParameters struct {
 	// GroupName is the group name (known as Description in the GUI).
 	//+optional
 	//+kubebuilder:validation:MaxLength=64
-	GroupName *string `json:"groupName"`
+	GroupName *string `json:"groupName,omitempty"`
 	//+optional
 	// LDAPEnabled determines whether LDAP authentication is enabled for members of this group.
-	LDAPEnabled *bool `json:"ldapEnabled"`
+	LDAPEnabled *bool `json:"ldapEnabled,omitempty"`
 	//+optional
 	// LDAPGroup us the group's name from the LDAP system.
-	LDAPGroup *string `json:"ldapGroup"`
+	LDAPGroup *string `json:"ldapGroup,omitempty"`
 	//+optional
-	LDAPMatchAttribute *string `json:"ldapMatchAttribute"`
+	LDAPMatchAttribute *string `json:"ldapMatchAttribute,omitempty"`
 	//+optional
-	LDAPSearch *string `json:"ldapSearch"`
+	LDAPSearch *string `json:"ldapSearch,omitempty"`
 	// LDAPSearchUserBase specifies the LDAP search base from which the CMC should start when retrieving the user's LDAP record in order to apply filtering.
 	//+optional
-	LDAPSearchUserBase *string `json:"ldapSearchUserBase"`
+	LDAPSearchUserBase *string `json:"ldapSearchUserBase,omitempty"`
 	//+optional
 	// LDAPServerURL specifies the URL that the CMC should use to access the LDAP Server when authenticating users in this group.
-	LDAPServerURL *string `json:"ldapServerURL"`
+	LDAPServerURL *string `json:"ldapServerURL,omitempty"`
 	// LDAPUserDNTemplate specifies how users within this group will be authenticated against the LDAP system when they log into the CMC.
 	//+optional
-	LDAPUserDNTemplate *string `json:"ldapUserDNTemplate"`
+	LDAPUserDNTemplate *string `json:"ldapUserDNTemplate,omitempty"`
 	//+optional
-	S3EndpointsHTTPS []string `json:"s3endpointshttp"`
+	S3EndpointsHTTPS []string `json:"s3endpointshttp,omitempty"`
 	//+optional
-	S3EndpointsHTTP []string `json:"s3endpointshttps"`
+	S3EndpointsHTTP []string `json:"s3endpointshttps,omitempty"`
 	//+optional
-	S3WebsiteEndpoints []string `json:"s3websiteendpoints"`
+	S3WebsiteEndpoints []string `json:"s3websiteendpoints,omitempty"`
 }
 
 // GroupObservation are the observable fields of a Group.
