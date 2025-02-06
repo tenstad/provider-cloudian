8f59bc31100e0ec844c08494d32d019827938cdd
Amund Tenstad <github@amund.io>
2025-02-06 12:31:27 +0100

    chore: generate

--- apis/user/v1alpha1/zz_generated.managed.go
+++ apis/user/v1alpha1/zz_generated.managed.go
@@ -258,3 +258,63 @@ func (mg *User) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo
 func (mg *User) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
 	mg.Spec.WriteConnectionSecretToReference = r
 }
+
+// GetCondition of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetManagementPolicies of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetManagementPolicies() xpv1.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetPublishConnectionDetailsTo of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
+// GetWriteConnectionSecretToReference of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetManagementPolicies of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetManagementPolicies(r xpv1.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetPublishConnectionDetailsTo of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
+}
+
+// SetWriteConnectionSecretToReference of this UserQualityOfServiceLimits.
+func (mg *UserQualityOfServiceLimits) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
