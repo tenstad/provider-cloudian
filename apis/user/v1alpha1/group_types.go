d6fe2b7bfce9408907f829ceb5b20e087e30f2fb
Amund Tenstad <github@amund.io>
2025-02-06 15:20:47 +0100

    chore: update api descriptions (#134)

--- apis/user/v1alpha1/group_types.go
+++ apis/user/v1alpha1/group_types.go
@@ -75,7 +75,7 @@ type GroupStatus struct {
 
 // +kubebuilder:object:root=true
 
-// A Group is an example API type.
+// Group represents a Cloudian group.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
